097190e526b7a30409b6d7aa8c386189ff4684a7
thucnu <thucnu0608@gmail.com>
2025-09-23 20:51:13 +0700

    init source

---
+++ src/api/axios.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:5000/api",
+});
+
+// Optional: Add token to every request if exists
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default instance;
